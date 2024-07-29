da9c1baad89672eb3470f174e5fe19043f9f331d
Alexander Coronado <alexandercoronado027@gmail.com>
2024-07-29 18:52:25 -0500

    Header, navs, animations, home and services

---
+++ app/resume/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+//imports
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+//about data
+const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing Lorem ipsum dolor sit amet consectetur adipisicing..",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Alexander Coronado",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+1) 504 327 8571",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+  ],
+};
+
+const page = () => {
+  return <div>resume page</div>;
+};
+
+export default page;
